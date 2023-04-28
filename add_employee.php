b0522f716a95f3ec06742520978cf375e734a378
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-04-29 02:48:53 +0600

    Add files via upload

--- add_employee.php
+++ add_employee.php
@@ -169,7 +169,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
                     </div>
         
                     <br> 
-                    <button type="submit" name="submit" class="btn btn-primary">Add Employee</button>
+                    <button type="submit" name="submit" class="btn btn-danger">Submit</button>
                     <br>
                 </form>
             </div>
