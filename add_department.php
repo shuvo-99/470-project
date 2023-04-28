b17e6b73cc83e3aa4a7801b3dce77f0f594aa559
Andalib Rahman Shagoto <64375438+Shagoto20101195@users.noreply.github.com>
2023-04-28 12:22:01 +0600

    Add files via upload

---
+++ add_department.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include("dbconnection.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $dept_name = $_POST['dept_name'];
+    $dept_head = $_POST['dept_head'];
+    $dept_task = $_POST['dept_task'];
+    $no_of_members = $_POST['no_of_members'];
+
+    $query = "select * from department where dept_name = '$dept_name'";
+    $result = mysqli_query($connect, $query);
+    if(mysqli_num_rows($result) == 0)
+    {
+        $query = "insert into department values(0, '$dept_name', '$dept_head', '$dept_task', '$no_of_members')";
+        $result = mysqli_query($connect, $query);
+    }
+    else
+    {
+        $text = "Department Already Exists!";
+        echo("<script type=text/javascript>alert('$text');</script>");
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Department</title>
+</head>
+    
+<body>
+    <header>
+        
+    </header>
+
+    <main>
+        <form method="POST">
+            <input type="text" name="dept_name" placeholder="Department Name" required><br>
+            <input type="text" name="dept_head" placeholder="Department Head" required><br>
+            <input type="text" name="dept_task" placeholder="Department Task" required><br>
+            <input type="number" name="no_of_members" placeholder="Number of Members" required><br>
+            <input type="submit" value="Add Department">
+        </form>
+    </main>
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
---
+++ add_employee.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+include("dbconnection.php");
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $username = $_POST['username'];
+    $emp_name = $_POST['emp_name'];
+    $password = $_POST['password'];
+    $mobile_no = $_POST['mobile_no'];
+    $birth_year = $_POST['birth_year'];
+    $dept_name = $_POST['dept_name'];
+    $emp_position = $_POST['emp_position'];
+    $gender = $_POST['gender'];
+    $salary = $_POST['salary'];
+    $joining_year = $_POST['joining_year'];
+
+    $query = "select * from user where username = '$username'";
+    $result = mysqli_query($connect, $query);
+    if(mysqli_num_rows($result) == 0)
+    {
+        $query = "insert into employee values(0, '$username', '$emp_name', '$mobile_no', '$birth_year', '$emp_position', '$dept_name', '$gender', '$salary', '$joining_year')";
+        $result = mysqli_query($connect, $query);
+        $query = "insert into user values('$username', '$password')";
+        $result = mysqli_query($connect, $query);
+    }
+    else
+    {
+        $text = "Username Already Exists!";
+        echo("<script type=text/javascript>alert('$text');</script>");
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Employee</title>
+</head>
+    
+<body>
+    <header>
+        
+    </header>
+
+    <main>
+        <form method="POST">
+            <input type="text" name="username" placeholder="Username" required><br>
+            <input type="text" name="emp_name" placeholder="Employee Name" required><br>
+            <input type="password" name="password" placeholder="Password" required><br>
+            <input type="text" name="mobile_no" placeholder="Mobile Number" required><br>
+            <input type="text" name="birth_year" placeholder="Birth Year (Year-Month-Day)" required><br>
+            <input type="text" name="dept_name" placeholder="Department Name" required><br>
+            <input type="text" name="emp_position" placeholder="Employee Position" required><br>
+            <input type="text" name="gender" placeholder="Gender" required><br>
+            <input type="text" name="salary" placeholder="Salary" required><br>
+            <input type="text" name="joining_year" placeholder="Joining Year (Year-Month-Day)" required><br>
+            <input type="submit" value="Add Employee">
+        </form>
+    </main>
+
+    <footer>
+        
+    </footer>
+</body>
+</html>
