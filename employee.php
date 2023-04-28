fca1f2655147a349da1a88f834e84065e3fb1a0b
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-04-28 12:11:57 +0600

    Add files via upload

---
+++ employee.php
@@ -0,0 +1,94 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Employee</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- <h1>Hello, world!</h1> -->
+    <!-- <nav class="navbar bg-light ">
+      <div class="container-fluid">
+        <a class="navbar-brand ">Human Resource Management System</a>
+        <a class="navbar-brand" href="employee.php">Profile</a>
+        <a class="navbar-brand" href="admin_login.php">Logout</a>
+      </div>
+    </nav> -->
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div>
+        <a href="employee.php" class="btn" role="button">Human Resource Management System</a>
+        </div>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarScroll"
+        >
+          <ul
+            class="navbar-nav me-2 my-2 my-lg-0 navbar-nav-scroll"
+            style="--bs-scroll-height: 100px"
+          >
+            <li class="nav-item nav-text">
+              <a href="employee.php" class="btn" role="button">Profile</a>
+            </li>
+            <li class="nav-item nav-text">
+              <a href="inbox.php" class="btn" role="button">Inbox</a>
+            </li>
+            <li class="nav-item nav-text">
+              <a href="donation_history.php" class="btn" role="button"
+                >Donation history</a
+              >
+            </li>
+            <li class="nav-item nav-text">
+              <a href="view_request.php" class="btn" role="button"
+                >Donation Request</a
+              >
+            </li>
+            <li class="nav-item">
+              <a href="logout.php" class="btn btn-warning" role="button"
+                >Log Out</a
+              >
+            </li>
+          </ul>
+          <!-- <form class="d-flex">
+            
+            <li class="nav-item">
+              <a class="nav-link disabled">Link</a>
+            </li>
+            <a href="logout.php" class="btn btn-warning" role="button"
+              >Log Out</a
+            >
+          </form> -->
+        </div>
+      </div>
+    </nav>
+    <main>
+      <section class="container">
+        <div class="col-lg-3">
+          <p>function</p>
+        </div>
+        <div class="col-lg-9">
+          <p>profile</p>
+        </div>
+      </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
+
