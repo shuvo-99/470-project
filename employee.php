addd2d7c1698e06d9ffb5b3bdd5aa58735e865d2
Nirjhor Datta <97627606+nirjhor123@users.noreply.github.com>
2023-04-28 16:53:21 +0600

    Add files via upload

--- employee.php
+++ employee.php
@@ -1,7 +1,8 @@
-<?php
 
-?>
-
+<?php 
+	include "dbconnection.php";
+	
+ ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -134,8 +135,100 @@
             </div>
             <div class="col-8">
               Welcome 
+                <br>
+                <?php
+                $q=mysqli_query($db,"SELECT * FROM employee  where username='shagoto_t14' ;");
+ 				$row=mysqli_fetch_assoc($q);
+
+ 	
+ 			?>
+            <?php
+ 				echo "<b>";
+ 				echo "<table class='table table-bordered'>";
+	 				echo "<tr>";
+	 					echo "<td>";
+	 						echo "<b>Employee Sl: </b>";
+	 					echo "</td>";
+
+	 					echo "<td>";
+	 						echo $row['emp_sl'];
+	 					echo "</td>";
+	 				echo "</tr>";
+
+
+                     echo "<tr>";
+	 					echo "<td>";
+	 						echo "<b> Name: </b>";
+	 					echo "</td>";
+	 					echo "<td>";
+	 						echo $row['emp_name'];
+	 					echo "</td>";
+	 				echo "</tr>";
+                     echo "<tr>";
+	 					echo "<td>";
+	 						echo "<b> Employee Position: </b>";
+	 					echo "</td>";
+	 					echo "<td>";
+	 						echo $row['emp_position'];
+	 					echo "</td>";
+	 				echo "</tr>";
+
+
+                     echo "<tr>";
+	 					echo "<td>";
+	 						echo "<b> Email: </b>";	
+	 					echo "</td>";
+	 					echo "<td>";
+	 						echo $row['admin_email'];
+	 					echo "</td>";
+	 				echo "</tr>";
+
+
+	 				echo "<tr>";
+	 					echo "<td>";
+	 						echo "<b>Mobile No </b>";
+	 					echo "</td>";
+	 					echo "<td>";
+	 						echo $row['mobile_no'];
+	 					echo "</td>";
+	 				echo "</tr>";
+                     echo "<tr>";
+                     echo "<td>";
+                         echo "<b>Department_ Name: </b>";
+                     echo "</td>";
+                     echo "<td>";
+                         echo $row['dept_name'];
+                     echo "</td>";
+                 echo "</tr>";
+                 echo "<tr>";
+                 echo "<td>";
+                     echo "<b> Birth Year: </b>";
+                 echo "</td>";
+                 echo "<td>";
+                     echo $row['birth_year'];
+                 echo "</td>";
+             echo "</tr>";	
+             echo "<tr>";
+             echo "<td>";
+                 echo "<b> Salary: </b>";
+             echo "</td>";
+             echo "<td>";
+                 echo $row['salary'];
+             echo "</td>";
+         echo "</tr>";
+         echo "<tr>";
+         echo "<td>";
+             echo "<b> Gemder: </b>";
+         echo "</td>";
+         echo "<td>";
+             echo $row['gender'];
+         echo "</td>";
+     echo "</tr>";
+ 				echo "</table>";
+ 				echo "</b>";
+ 			?>
             </div>
-            
+           
           </div>
         </div>
       </section>
@@ -309,4 +402,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
-
