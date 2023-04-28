783029807c56954fbf5b59cdb5773768abfe17b9
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-04-28 15:32:57 +0600

    Add files via upload

--- employee.php
+++ employee.php
@@ -9,6 +9,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Employee</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  
+    <!-- font awesome -->
+
+    <script
+      src="https://kit.fontawesome.com/5b05054594.js"
+      crossorigin="anonymous"
+    ></script>
   </head>
   <body>
     <!-- <h1>Hello, world!</h1> -->
@@ -83,15 +90,199 @@
         <div class="container text-center">
           <div class="row">
             <div class="col-4 text-white bg-secondary">
-              1 of 3
+              <!-- <div class="row flex-column container bg-primary ps-2">
+                <div class="col">
+                  1 of 2
+                </div>
+                <div class="col">
+                  2 of 2
+                </div>
+              </div>
+              </div> -->
+              <div class="row container bg-primary">
+                <div class="col">
+                  1
+                </div>
+              </div>
+              <br>
+              <div class="row container">
+                <div class="col">
+                  <button type="button" class="btn btn-danger">View Leave Report</button>
+                </div>
+              </div>
             </div>
             <div class="col-8">
-              2 of 3 (wider)
+              Profile
             </div>
           </div>
         </div>
       </section>
     </main>
+    <footer>
+      <section class="bg-dark">
+        <div class="container my-5">
+          <div class="row gx-5 gy-5">
+            <!-- About me -->
+
+            <div class="col-lg-4">
+              <!-- <div class="p-3 rounded-3 shadow-lg text-center h mb-5">
+                Experience
+              </div> -->
+              <h2 class="fw-bold text-white">About Us</h2>
+              <p class="text-white">
+                Sunrise company is a leading IT company in Bangladesh with over 150+ clients from different countries accross the world. 
+              </p>
+            </div>
+
+            <!-- Links -->
+
+            <div class="col-lg-4">
+              <h1 class="text-white">What makes us unique</h1>
+              <ul class="p-0">
+                <!-- home -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <a href="#home">
+                      <i class="fas fa-arrow-right text-white"></i>
+                    </a>
+                  </div>
+                  <div class="text-white ps-3">
+                    <a class="flink text-white">Multinational team</a>
+                  </div>
+                </li>
+
+                <!-- About Me -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <a href="#aboutme">
+                      <i class="fas fa-arrow-right text-white"></i>
+                    </a>
+                  </div>
+                  <div class="text-white ps-3">
+                    <a class=" text-white">Interdisciplinary skillset</a>
+                  </div>
+                </li>
+
+                <!-- Experience -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <a href="#ex">
+                      <i class="fas fa-arrow-right text-white"></i>
+                    </a>
+                  </div>
+                  <div class="text-white ps-3">
+                    <a  class="flink text-white">Equal opportunity employer</a>
+                  </div>
+                </li>
+
+                <!-- Projects -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <a href="#project">
+                      <i class="fas fa-arrow-right text-white"></i>
+                    </a>
+                  </div>
+                  <div class="text-white ps-3">
+                    <a class="flink text-white">Rapid solution development</a>
+                  </div>
+                </li>
+
+                <!-- Skill -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <a href="#skill">
+                      <i class="fas fa-arrow-right text-white"></i>
+                    </a>
+                  </div>
+                  <div class="text-white ps-3">
+                    <a href="#skill" class="flink text-white">Customer and user centered</a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+            <!-- Conatacts and Info -->
+
+            <div class="col-lg-4">
+              <h1 class="text-white">Contacts and Info</h1>
+              <ul class="p-0">
+                <!-- House -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <i class="fas fa-map-marker-alt text-white"></i>
+                  </div>
+                  <div class="text-white ps-3">
+                    House - 12, Block - C, Road - 01 Gulshan-01, Dhaka
+                  </div>
+                </li>
+
+                <!-- phone -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <i class="fas fa-phone-alt text-white"></i>
+                  </div>
+                  <div class="text-white ps-3">987******</div>
+                </li>
+
+                <!-- gmail -->
+
+                <li class="py-2 d-flex">
+                  <div>
+                    <i class="fas fa-envelope text-white"></i>
+                  </div>
+                  <div class="text-white ps-3">sunrise*****@gmail.com</div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <!-- social Links -->
+
+        <div class="container mt-5 pb-5">
+          <ul class="d-flex justify-content-center align-items-center m-0 p-0">
+            
+            <li class="py-2 pe-4 d-flex">
+              <a
+                target="_blank"
+                href=""
+              >
+                <i class="fab fa-linkedin text-white fa-2x"></i>
+              </a>
+            </li>
+            <li class="py-2 pe-4 d-flex">
+              <a target="_blank" href="">
+                <i class="fab fa-behance-square text-white fa-2x"></i>
+              </a>
+            </li>
+            <li class="py-2 pe-4 d-flex">
+              <a
+                target="_blank"
+                href=""
+              >
+                <i class="fab fa-facebook-square text-white fa-2x"></i>
+              </a>
+            </li>
+            <li class="py-2 pe-4 d-flex">
+              <a target="_blank" href="">
+                <i class="fab fa-twitter-square text-white fa-2x"></i>
+              </a>
+            </li>
+          </ul>
+          <br />
+          <p class="text-white text-center">
+            © Copyright 2023 Sunrise Company | All rights Reserved
+          </p>
+        </div>
+      </section>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
