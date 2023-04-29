92571ea1d58d89235a74a25f472001580eeadeba
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-04-29 15:45:38 +0600

    Add files via upload

--- employeeleavereport.php
+++ employeeleavereport.php
@@ -22,15 +22,70 @@
       .func_btn{
         color: rgb(250, green, blue);
       }
+      .link-deco {
+        text-decoration: none;
+      }
     </style>
   </head>
-  
+    
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div class="container-fluid container">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div>
+        <a href="employee.php" class="btn" role="button">Human Resource Management System</a>
+        </div>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarScroll"
+        >
+          <ul
+            class="navbar-nav me-2 my-2 my-lg-0 navbar-nav-scroll"
+            style="--bs-scroll-height: 100px"
+          >
+            <!-- Profile button -->
+
+            <li class="nav-item nav-text">
+              <a href="employee.php" class="btn" role="button">Profile</a>
+            </li>
+            <!-- Logout button -->
+
+            <li class="nav-item">
+              <a href="logout.php" class="btn btn-warning" role="button"
+                >Log Out</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 <?php
 $q=mysqli_query($db,"SELECT * FROM leave_request");
 
 $row=mysqli_fetch_assoc($q);
 ?>
-<table class="table table-striped table-primary" >
+<section class="container">
+  <div>
+  <br>
+      <br>
+      <br>
+      <br>
+      <h3 class="text-center">My Leave Report</h3>
+      <br>
+  </div>
+</section>
+<table class="table table-striped table-primary table-bordered border-dark" >
             <thead>
             
             <tr >
@@ -55,7 +110,15 @@ $row=mysqli_fetch_assoc($q);
 								while ($row= mysqli_fetch_assoc($res)){
 								
 									echo "<tr><td>{$row["username"]}</td><td>{$row["emp_name"]}</td><td>{$row["reason"]}</td><td>{$row["no_of_days_requested"]}</td><td>{$row["status"]}</td></tr>";
-                                    
+                   ?> 
+                  <tr>
+                  
+                  <td class="text-center"><?php echo $row['username'] ?></td>
+                  <td class="text-center"><?php echo $row['emp_name'] ?></td>
+                  <td class="text-center"><?php echo $row['reason'] ?></td>
+                  <td class="text-center"><?php echo $row['no_of_days_reqested'] ?></td>
+                  <td class="text-center"><?php echo $row['status'] ?></td>
+                  </tr>  <?php                 
 								}
 								
 								echo "</table>";
@@ -65,3 +128,5 @@ $row=mysqli_fetch_assoc($q);
             
           </tbody>
         </table>
+</body>
+</html>
